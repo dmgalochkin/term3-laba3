1117e036d3f212347f0b6d33e88e645cb6609c0c
Dmitrii <dmgalochkin@gmail.com>
2025-11-02 23:13:51 +0300

    mnogo vsego

--- structures/TQueue.hpp
+++ structures/TQueue.hpp
@@ -1,46 +1,51 @@
 #pragma once
 template<typename T>
-class TStack
+class TQueue
 {
 private:
   int capacity;
   int size;
+  int front;
+  int rear;
   T* memory;
 public:
-  TStack();
-  TStack(const int capacity_);
-  TStack(const TStack<T>& other);
-  TStack(TStack<T>&& other);
-  ~TStack();
+  TQueue();
+  TQueue(const int capacity_);
+  TQueue(const TQueue<T>& other);
+  TQueue(TQueue<T>&& other);
+  ~TQueue();
 
-  TStack& operator=(const TStack<T>& other);
-  TStack& operator=(TStack<T>&& other);
+  TQueue& operator=(const TQueue<T>& other);
+  TQueue& operator=(TQueue<T>&& other);
 
   void Push(T value);
   void Pop();
-  T& Top();
+  T& Front();
+  T& Back();
 
   bool IsEmpty() const;
   bool IsFull() const;
   int GetSize() const;
   int GetCapacity() const;
   
-  bool operator==(const TStack<T>& other) const;
-  bool operator!=(const TStack<T>& other) const;
+  bool operator==(const TQueue<T>& other) const;
+  bool operator!=(const TQueue<T>& other) const;
   
   void Resize(const int newCapacity);
 };
 
 template<typename T>
-TStack<T>::TStack()
+TQueue<T>::TQueue()
 {
   capacity = 0;
   size = 0;
+  front = 0;
+  rear = 0;
   memory = nullptr; 
 }
 
 template<typename T>
-TStack<T>::TStack(const int capacity_)
+TQueue<T>::TQueue(const int capacity_)
 {
   if (capacity_ < 0)
   {
@@ -48,6 +53,8 @@ TStack<T>::TStack(const int capacity_)
   }
   capacity = capacity_;
   size = 0;
+  front = 0;
+  rear = 0;
   if (capacity != 0) 
   {
     memory = new T[capacity];
@@ -59,74 +66,54 @@ TStack<T>::TStack(const int capacity_)
 }
 
 template<typename T>
-TStack<T>::TStack(const TStack<T>& other)
+TQueue<T>::TQueue(const TQueue<T>& other)
 {
   capacity = other.capacity;
   size = other.size;
-  if (capacity != 0)
-  {
-    memory = new T[capacity];
-  for (int i = 0; i < size; ++i)
-    {
-      memory[i] = other.memory[i];
-    }
-  }
-  else
-  {
-    memory = nullptr;
-  }
-}
-
-template<typename T>
-TStack<T>::TStack(TStack<T>&& other)
-{
-  capacity = other.capacity;
-  size = other.size;
-  memory = other.memory;
-  other.memory = nullptr;
-}
-
-template<typename T>
-TStack<T>::~TStack()
-{
-  if (memory != nullptr)
-  {
-    delete[] memory;
-  }
-}
-
-template<typename T>
-TStack<T>& TStack<T>::operator=(const TStack<T>& other)
-{
-  if (this == &other)
-  {
-    return *this;
-  }
+  front = 0;
+  rear = size > 0 ? size - 1 : 0;
   
-  if (memory != nullptr)
-  {
-    delete[] memory;
-  }
-  
-  capacity = other.capacity;
-  size = other.size;
   if (capacity != 0)
   {
     memory = new T[capacity];
     for (int i = 0; i < size; ++i)
     {
-      memory[i] = other.memory[i];
+      int sourceIndex = (other.front + i) % other.capacity;
+      memory[i] = other.memory[sourceIndex];
     }
   }
   else
   {
     memory = nullptr;
   }
-  return *this;
 }
 
 template<typename T>
-TStack<T>& TStack<T>::operator=(TStack<T>&& other)
+TQueue<T>::TQueue(TQueue<T>&& other)
+{
+  capacity = other.capacity;
+  size = other.size;
+  front = other.front;
+  rear = other.rear;
+  memory = other.memory;
+  other.memory = nullptr;
+  other.capacity = 0;
+  other.size = 0;
+  other.front = 0;
+  other.rear = 0;
+}
+
+template<typename T>
+TQueue<T>::~TQueue()
+{
+  if (memory != nullptr)
+  {
+    delete[] memory;
+  }
+}
+
+template<typename T>
+TQueue<T>& TQueue<T>::operator=(const TQueue<T>& other)
 {
   if (this == &other)
   {
@@ -140,69 +127,139 @@ TStack<T>& TStack<T>::operator=(TStack<T>&& other)
   
   capacity = other.capacity;
   size = other.size;
-  memory = other.memory;
-  other.memory = nullptr;
-  other.capacity = 0;
-  other.size = 0;
+  front = 0;
+  rear = size > 0 ? size - 1 : 0;
+  
+  if (capacity != 0)
+  {
+    memory = new T[capacity];
+    for (int i = 0; i < size; ++i)
+    {
+      int sourceIndex = (other.front + i) % other.capacity;
+      memory[i] = other.memory[sourceIndex];
+    }
+  }
+  else
+  {
+    memory = nullptr;
+  }
   return *this;
 }
 
 template<typename T>
-void TStack<T>::Push(T value)
+TQueue<T>& TQueue<T>::operator=(TQueue<T>&& other)
+{
+  if (this == &other)
+  {
+    return *this;
+  }
+  
+  if (memory != nullptr)
+  {
+    delete[] memory;
+  }
+  
+  capacity = other.capacity;
+  size = other.size;
+  front = other.front;
+  rear = other.rear;
+  memory = other.memory;
+  
+  other.memory = nullptr;
+  other.capacity = 0;
+  other.size = 0;
+  other.front = 0;
+  other.rear = 0;
+  
+  return *this;
+}
+
+template<typename T>
+void TQueue<T>::Push(T value)
 {
   if (size == capacity)
   {
-    throw "Stack is full";
+    throw "Queue is full";
   }
-  memory[size++] = value;
+  
+  if (size == 0)
+  {
+    front = 0;
+    rear = 0;
+  }
+  else
+  {
+    rear = (rear + 1) % capacity;
+  }
+  
+  memory[rear] = value;
+  ++size;
 }
 
 template<typename T>
-void TStack<T>::Pop()
+void TQueue<T>::Pop()
 {
   if (size == 0)
   {
-    throw "Stack is empty";
+    throw "Queue is empty";
   }
+  
+  front = (front + 1) % capacity;
   --size;
+  
+  if (size == 0)
+  {
+    front = 0;
+    rear = 0;
+  }
 }
 
 template<typename T>
-T& TStack<T>::Top()
+T& TQueue<T>::Front()
 {
   if (size == 0)
   {
-    throw "Stack is empty";
+    throw "Queue is empty";
   }
-  return memory[size - 1];
+  return memory[front];
 }
 
 template<typename T>
-bool TStack<T>::IsEmpty() const
+T& TQueue<T>::Back()
+{
+  if (size == 0)
+  {
+    throw "Queue is empty";
+  }
+  return memory[rear];
+}
+
+template<typename T>
+bool TQueue<T>::IsEmpty() const
 {
   return size == 0;
 }
 
 template<typename T>
-bool TStack<T>::IsFull() const
+bool TQueue<T>::IsFull() const
 {
   return size == capacity;
 }
 
 template<typename T>
-int TStack<T>::GetSize() const
+int TQueue<T>::GetSize() const
 {
   return size;
 }
 
 template<typename T>
-int TStack<T>::GetCapacity() const
+int TQueue<T>::GetCapacity() const
 {
   return capacity;
 }
 
 template<typename T>
-void TStack<T>::Resize(const int newCapacity)
+void TQueue<T>::Resize(const int newCapacity)
 {
   if (newCapacity < 0)
   {
@@ -215,12 +272,17 @@ void TStack<T>::Resize(const int newCapacity)
   
   T* oldMemory = memory;
   int oldSize = size;
+  int oldFront = front;
+  int oldCapacity = capacity;
   
   capacity = newCapacity;
   if (size > capacity)
   {
     size = capacity;
   }
+  
+  front = 0;
+  rear = size > 0 ? size - 1 : 0;
   
   if (capacity != 0)
   {
@@ -229,7 +291,8 @@ void TStack<T>::Resize(const int newCapacity)
     {
       for (int i = 0; i < size; ++i)
       {
-        memory[i] = oldMemory[i];
+        int sourceIndex = (oldFront + i) % oldCapacity;
+        memory[i] = oldMemory[sourceIndex];
       }
     }
   }
@@ -237,6 +300,7 @@ void TStack<T>::Resize(const int newCapacity)
   {
     memory = nullptr;
   }
+  
   if (oldMemory != nullptr)
   {
     delete[] oldMemory;
@@ -244,16 +308,21 @@ void TStack<T>::Resize(const int newCapacity)
 }
 
 template<typename T>
-bool TStack<T>::operator==(const TStack<T>& other) const
+bool TQueue<T>::operator==(const TQueue<T>& other) const
 {
   if (size != other.size)
   {
     return false;
   }
-  
+  if (capacity == 0 || other.capacity == 0)
+  {
+    return true;
+  }
   for (int i = 0; i < size; ++i)
   {
-    if (memory[i] != other.memory[i])
+    int thisIndex = (front + i) % capacity;
+    int otherIndex = (other.front + i) % other.capacity;
+    if (memory[thisIndex] != other.memory[otherIndex])
     {
       return false;
     }
@@ -262,7 +331,7 @@ bool TStack<T>::operator==(const TStack<T>& other) const
 }
 
 template<typename T>
-bool TStack<T>::operator!=(const TStack<T>& other) const
+bool TQueue<T>::operator!=(const TQueue<T>& other) const
 {
   return !(*this == other);
 }
